a6a76dee3b70ea7b0a766de67e2d2322d44d6c2d
gantar22 <nathancarterwilliams@gmail.com>
2019-10-05 15:12:49 -0400

    Text mesh pro twine

---
+++ Assets/UI/wavy_text.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(TMPro.TMP_Text))]
+public class wavy_text : MonoBehaviour
+{
+    TMPro.TMP_Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<TMPro.TMP_Text>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
