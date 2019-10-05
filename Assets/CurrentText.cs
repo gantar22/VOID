6938c903d3d0c0bb77efe7a0bddab97827fd2b86
Malek <maleka@qatar.cmu.edu>
2019-10-06 00:41:10 +0300

    script

---
+++ Assets/CurrentText.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public enum flags { };
+
+
+[System.Serializable]
+public struct Line {
+
+    [TextArea]
+    public string text;
+
+}
+
+[System.Serializable]
+public struct Choice {
+
+    public string choice;
+    public CurrentText dest;
+    public flags[] required;
+}
+
+[CreateAssetMenu(menuName = "Text")]
+public class CurrentText : ScriptableObject
+{
+
+    public Line[] spoken;
+    public Choice[] choices;
+
+}
