4d1ac4a12c01a4bb8d904e89c8306efa7e647fdc
gantar22 <nathancarterwilliams@gmail.com>
2019-10-05 17:45:58 -0400

    merging

---
+++ Assets/UI/DialogueWalker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueWalker : MonoBehaviour
+{
+    [SerializeField] private TMPro.TMP_Text spokenPrefab;
+
+    [SerializeField] private Button choicePrefab;
+
+    [SerializeField] private CurrentText startingText;
+    
+    [SerializeField] private VerticalLayoutGroup textHolder;
+
+    IEnumerator blitText()
+    {
+        foreach (var VARIABLE in )
+        {
+            
+        }
+        
+        yield return null;
+    }
+}
