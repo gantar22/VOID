e9eb28949d6190dfb6fcfb347d790d186d1162cc
gantar22 <nathancarterwilliams@gmail.com>
2019-10-07 01:48:42 -0400

    pretties

--- Assets/UI/wavy_text.cs
+++ Assets/UI/wavy_text.cs
@@ -10,12 +10,18 @@ public class wavy_text : MonoBehaviour
     void Start()
     {
         text = GetComponent<TMPro.TMP_Text>();
-       
+        StartCoroutine(wave());
+
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator wave()
     {
-        
+        float time = Time.time;
+        while (true)
+        {
+            time += Time.deltaTime * Random.value * (Mathf.Pow(transform.position.x * transform.position.y,2) % .5f + .5f);
+            text.outlineWidth = Mathf.PingPong(time * .2f,1f);
+            yield return null;
+        }
     }
 }
